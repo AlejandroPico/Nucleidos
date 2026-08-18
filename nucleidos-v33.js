b43c203475d3674c1da1a7a2fcf28dfa5d2578fb
Alejandro Pico <115283749+AlejandroPico@users.noreply.github.com>
2026-08-18 15:06:00 +0200

    Bump final responsive release to 34.3.0

--- nucleidos-v33.js
+++ nucleidos-v33.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '34.2.0';
+  const VERSION = '34.3.0';
   const $ = (selector, root = document) => root.querySelector(selector);
   const $$ = (selector, root = document) => [...root.querySelectorAll(selector)];
 
